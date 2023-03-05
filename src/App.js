2cf88ddf51dce7a26be66526016dd41176c44b5f
Kuba Tkaczyk <mowmikubek@gmail.com>
2023-03-05 15:06:20 +0100

    Create base for filters

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import { useState } from 'react';
+
 import CardsContainer from './components/CardsContainer/CardsContainer';
 import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+
 // import './App.css';
 
 function App() {
+  const [flagsData, setFlagsData] = useState({})
+  const [searchValue, setSearchValue] = useState('')
+
   return (
     <>
       <Header />
-      <main>
-        <CardsContainer />
-      </main>
+      <Navbar text={searchValue} setText={setSearchValue} />
+      <CardsContainer 
+        flagsData={flagsData} 
+        setFlagsData={setFlagsData} 
+        filter={searchValue} />
     </>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import Searchbar from './Searchbar/Searchbar'
+import styles from './styles.module.css'
+
+export default function Navbar(props) {
+    return (
+        <nav className={styles.navbar}>
+            {/* <Searchbar text={props.text} setText={props.setText} /> */}
+            {/* <Searchbar /> */}
+        </nav>
+    )
+}
