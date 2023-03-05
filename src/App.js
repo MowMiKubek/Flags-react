ae0f7fba4e29627b79b5d6e67bcfa4ccd5c04d0f
Kuba Tkaczyk <mowmikubek@gmail.com>
2023-03-05 10:57:17 +0100

    Create header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import CardsContainer from './components/CardsContainer/CardsContainer';
-import './App.css';
+import Header from './components/Header/Header';
+// import './App.css';
 
 function App() {
   return (
-    <main>
-      <h1>App</h1>
-      <CardsContainer />
-    </main>
+    <>
+      <Header />
+      <main>
+        <CardsContainer />
+      </main>
+    </>
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import styles from './styles.module.css'
+import Logo from '../../moon.svg'
+export default function Header (props) {
+    return (
+        <header className={styles.header}>
+            <h1>Where in the world?</h1>
+                <h2>
+                    <img 
+                    src={Logo}
+                    style={{transform: "scale(1.5) translate(-4px, -2px)"}}
+                    />
+                    Dark mode
+                </h2>
+        </header>
+    )
+}
